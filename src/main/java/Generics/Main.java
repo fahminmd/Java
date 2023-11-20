e9d0d8609ec21316cfcdd5acfc030474f8787ca9
fahmin <fahminmd@gmail.com>
2023-11-20 15:45:25 -0600

    Generics simple example added

--- src/main/java/Generics/Main.java
+++ src/main/java/Generics/Main.java
@@ -2,6 +2,12 @@ package Generics;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world from Generics!");
+      //  new GenericsSample<Integer>().add(1); or in below another way to declare
+        var list = new GenericsSample<Integer>();
+        list.add(1);
+        int number = list.get(0);
+        System.out.println("Here is the saved integer  : "+ number );
     }
 }
